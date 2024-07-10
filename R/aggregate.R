6beab6acb4df1b1edd30a9585860c87ac45007b3
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2024-07-10 20:41:46 +0100

    revising verbs ahead of updating vignette

--- R/aggregate.R
+++ R/aggregate.R
@@ -1,85 +1,201 @@
 
-#' @title Transform Parameter Function into Values by Age Bin
+make_partition <- function(
+  ..., open_partition
+) {
+  partition <- c(...)
+  if (open_partition[1]) partition <- c(-Inf, partition)
+  if (open_partition[2]) partition <- c(partition, Inf)
+  return(unique(sort(partition)))
+}
+
+make_density <- function(densities, interpolater = approxfun, ...) {
+  if (is.function(densities)) {
+    return(densities)
+  } else {
+    return(approxfun(
+      c(densities$from, partitions[length(partitions)]),
+      c(densities$weight, 0), ...
+    ))
+  }
+}
+
+make_weight <- function(f_params, f_density) {
+  return(function(x, ...) f_param(x, ...) * f_density(x))
+}
+
+#' @title Transform Parameter Function into Values by Partition
 #'
 #' @description
-#' `aggregate_param` integrates a functional relationship between some model
-#' feature and age, for example case fatality ratio, into static parameter
-#' values for use in a lower-resolution age-binned compartmental model.
+#' `blend` integrates a functional relationship between some model
+#' feature, for example case fatality ratio, into static parameter
+#' values for use in a lower-resolution partitioned compartmental model.
 #'
-#' @param f_age a function, `f(a, ...)` which takes age `a` as the first argument
-#' and optionally any other `...`. `f` should be vectorized in `a`
+#' @param f_param a function, `f(x, ...)` which operates on the feature
+#' (e.g. age), and optionally additional arguments, and yields the parameter
+#' value.
 #'
-#' @param target_bins a vector, the cutoffs for each age bin. For example, a vector
-#' `ages <- c(0, 5, 20, 100)` would correspond to intervals of `[0, 5)`, `[5, 20)`,
-#' and `[20, 100]`. Note the first value is a start (rather than cutoff) and
-#' that upper limits are exclusive, *except* for the final value.
+#' @param model_partition a numeric vector of cut points, which define the partition
 #'
-#' @param population a data.frame, the population by age.
+#' @param densities a `data.frame` with numeric columns `from` and `weight`. The
+#' `weight` column must be positive, but need not be normalized. Alternatively:
+#' a density function
 #'
-#' @return a vector,
+#' @param ... optional arguments to `f_param`
 #'
+#' @param open_partitions a logical vector, `length(open_partition) == 2`.
+#' Whether the `partitions` argument should be evaluated as open on lower and/or
+#' upper ends - i.e. should `-Inf` and/or `Inf` be pre/appended to the
+#' `partition` argument. n.b., this can also be accomplished by directly
+#' including the `Inf`s in the partition.
+#'
+#' @return a vector of parameter values, one for each partition
+#'
+#' @examples
+#' ifr_levin <- function(age_in_years) {
+#'   scaled <- exp(-7.56 + 0.121 * age_in_years)
+#'   scaled / (100 + scaled)
+#' }
+#' age_limits <- c(seq(0, 69, by = 5), 70, 80, 100)
+#' age_pyramid <- data.frame(
+#'   from = 0:99, weight = ifelse(0:99 < 65, 1, .99^(0:99-64))
+#' ) # flat age distribution, then 1% annual deaths
+#' ifr_blend <- blend(ifr_levin, age_limits, age_pyramid)
+#' # the actual function
+#' plot(
+#'   60:100, ifr_levin(60:100),
+#'   xlab = "age (years)", ylab = "IFR", type = "l"
+#' )
+#' # the properly aggregated blocks
+#' lines(
+#'   age_limits, c(ifr_blend, tail(ifr_blend, 1)),
+#'   type = "s", col = "dodgerblue"
+#' )
+#' # naively aggregated blocks
+#' ifr_naive <- ifr_levin(head(age_limits, -1) + diff(age_limits)/2)
+#' lines(
+#'   age_limits, c(ifr_naive, tail(ifr_naive, 1)),
+#'   type = "s", col = "firebrick"
+#' )
+#' # properly aggregated, but not accounting for distribution
+#' ifr_unif <- blend(ifr_levin, age_limits, within(age_pyramid, weight <- 1))
+#' lines(
+#'   age_limits, c(ifr_unif, tail(ifr_unif, 1)),
+#'   type = "s", col = "darkgreen"
+#' )
 #' @export
-aggregate_param <- function(
-  f_age,
-  target_bins,
-  population,
-  ...
+blend <- function(
+  f_param,
+  model_partition,
+  densities,
+  ...,
+  open_partition = c(lower = FALSE, upper = FALSE)
 ) {
-  
-  if(!0 %in% target_bins){target_bins <- c(0, target_bins)}
-  population[, mid := (lower+upper+1)/2]
-  population[, f_age_val := f_age(mid)]
-  population[, age_group := 0]
-  for(i in 1:length(target_bins)){
-    population[, age_group := age_group + (upper>target_bins[i])]
+
+  partitions <- make_partition(model_partition, open_partition)
+
+  # TODO argument checking
+
+  lowers <- head(partitions, -1)
+  uppers <- tail(partitions, -1)
+
+  f_density <- make_density(densities, method = "constant")
+
+  f <- make_weight(f_param, f_density)
+
+  out <- numeric(length(lowers))
+
+  for (i in seq_along(lowers)) {
+    out[i] <- integrate(
+      f, lower = lowers[i], upper = uppers[i], ...
+    )$value / integrate(
+      f_density, lower = lowers[i], upper = uppers[i], ...
+    )$value
   }
-  
-  out <- population[,.(agg_value = weighted.mean(f_age_val, w=pop)), by='age_group']
-  
+
   return(out)
 
 }
 
-#' @title Disaggregate a Compartmental Model Age-Based Outcome
+#' @title Create an Model Outcome Re-mapping
+#'
+#' @inheritParams blend
+#'
+#' @param new_partition the new partition of the underlying feature
+#'
+#' @return a `data.frame` which maps fractions of the original model partitions
+#' to the desired partitions, according to underlying relative outcome rates and
+#' densities
+#'
+#' @examples
+#' ifr_levin <- function(age_in_years) {
+#'   scaled <- exp(-7.56 + 0.121 * age_in_years)
+#'   scaled / (100 + scaled)
+#' }
+#' age_limits <- c(seq(0, 69, by = 5), 70, 80, 100)
+#' age_pyramid <- data.frame(
+#'   from = 0:99, weight = ifelse(0:99 < 65, 1, .99^(0:99-64))
+#' ) # flat age distribution, then 1% annual deaths
+#' ifr_alembic <- alembic(ifr_levin, age_pyramid, age_limits, 0:100)
+#'
+#' @export
+alembic <- function(
+  f_param,
+  densities,
+  model_partition,
+  new_partition,
+  ...,
+  open_partition = c(lower = FALSE, upper = FALSE)
+) {
+
+  overall_partition <- make_partition(
+    model_partition, new_partition, open_partition = open_partition
+  )
+
+  lowers <- head(overall_partition, -1)
+  uppers <- tail(overall_partition, -1)
+
+  f <- make_weight(f_param, make_density(densities, method = "constant"))
+
+  return(data.table::data.table(from = lowers)[, {
+    model_part <- findInterval(from, model_partition)
+    new_part <- findInterval(from, new_partition)
+    weight <- numeric(length(from))
+    for (i in seq_along(weight)) {
+      weight[i] <- integrate(f, lowers[i], uppers[i])$value
+    }
+    weight <- weight / aggregate(weight, by = list(model_part), FUN = sum)[model_part,]$x
+    .(model_partition = model_part, new_partition = new_part, model_fraction = weight)
+  }])
+
+}
+
+#' @title Re-Map Compartmental Model Outcomes
 #'
 #' @description
-#' `disaggregate_value` takes a low-age resolution outcome, for example deaths,
+#' `distill` takes a low-age resolution outcome, for example deaths,
 #' and proportionally distributes that outcome into a higher age resolution for
 #' use in subsequent analyses like years-life-lost style calculations.
 #'
-#' @param value a numeric vector, the model output by age group
+#' @param outcomes a long-format `data.frame` with columns `model_part` and
+#' `value`, optionally others that will be preserved
 #'
-#' @param f_age the relative rate of the subject outcome, by high
-#' resolution age. Note, this should be the same `f_age` as in [aggregate_param()]
-#' for parameter / outcome pairs - for example, when the outcome is death and
-#' the parameter function is the age-specific case fatality ratio (which would be
-#' aggregated to give single lower age resolution compartmental rates).
+#' @param mapping see [alembic()]: a `data.frame` in accordance with the return
+#' of that function.
 #'
-#' @inheritParams aggregate_param
+#' @return a `data.frame` mirroring `outcomes`, but with
 #'
-disaggregate_value <- function(
-  value, f_age, target_bins, population, ...
+#' @export
+distill <- function(
+  outcomes, mapping
 ) {
-  
-  if(!0 %in% target_bins){target_bins <- c(0, target_bins)}
-  population[, interval := 1 + upper - lower]
-  population <- population[,lapply(.SD, rep, times=interval)]
-  population[, pop:=pop/interval]
-  population[, interval:=NULL]
-  population[, age:=(min(population$lower)):(max(population$upper))]
-  population[, f_age_val := f_age(age)]
-  population[, f_tot := f_age_val*pop]
-  population[, age_group := 0]
-  for(i in 1:length(target_bins)){
-    population[, age_group := age_group + (upper>target_bins[i])]
-  }
-  value_dt <- data.table(age_group = sort(unique(population$age_group)),
-                         vals = value)
-  population <- population[value_dt, on='age_group']
-  pop_sum <- population[, .(age_grp_f = sum(f_tot)), by=age_group]
-  population <- population[pop_sum, on='age_group']
-  population[, disagg_value := vals*f_tot/age_grp_f]
-  
-  return(population[, c('age','disagg_value')])
+
+  setDT(outcomes)
+  setDT(mapping)
+
+  return(outcomes[
+    mapping, on = .(model_partition)
+  ][, .(value = sum(value*model_fraction)),
+    by = c("new_partition", setdiff(names(outcomes), c("model_partition", "value")))
+  ])
 
 }
