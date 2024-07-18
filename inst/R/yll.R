bc30ddf99723144ce2f0b954aa2d50a61556ad0d
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2024-07-18 08:50:05 -0400

    implement yll stage, fix item in distill_summary

--- inst/R/yll.R
+++ inst/R/yll.R
@@ -3,11 +3,20 @@ require(data.table)
 
 .args <- if (interactive()) c(
   file.path("input", "lex.rds"),
-  file.path("output", "distill_GBR_SC2.json"),
+  file.path("output", "distill_GBR_SC2.rds"),
+  "GBR",
   file.path("output", "yll_GBR_SC2.rds")
 ) else commandArgs(trailingOnly = TRUE)
 
+lex_dt <- readRDS(.args[1])
+lex_dt <- lex_dt[
+  iso3 == lex_dt[, match.arg(.args[3], unique(iso3))]
+]
 
-dt <- data.table()
+distill_dt <- readRDS(.args[2])
 
-dt |> saveRDS(tail(.args, 1))
+yll_dt <- distill_dt[
+  lex_dt, on = .(partition = age)
+][, .(YLL = sum(value*ex)), by = .(method)]
+
+yll_dt |> saveRDS(tail(.args, 1))
