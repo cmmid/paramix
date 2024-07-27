35b9120bfa4a3490bb4129fe1ba8558a4b0b0f50
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2024-07-26 21:54:48 -0400

    updated all simulations runs

---
+++ inst/R/disease_pars.R
@@ -0,0 +1,51 @@
+
+require(data.table)
+require(wpp2022)
+require(countrycode)
+
+.args <- if (interactive()) c(
+  file.path("input", "population.rds"),
+  file.path("input", "disease_pars.rda")
+) else commandArgs(trailingOnly = TRUE)
+
+# from Levin et al https://doi.org/10.1007/s10654-020-00698-1
+ifr_opts <- list(
+  SC2 = \(age_in_years) {
+    scaled <- exp(-7.56 + 0.121 * age_in_years)
+    scaled / (100 + scaled)
+  }
+)
+
+pop_dt <- readRDS(.args[1])
+
+# TODO a bit slow + repeated - move into own script?
+# for FLU, going to assume looks like all-cause mortality in an HIC setting
+ref_demo_dt <- pop_dt[iso3 == "GBR"]
+data("mx1dt")
+ref_mx_dt <- mx1dt[
+  year == 2021
+][,
+  iso3 := countrycode(country_code, "iso3n", "iso3c")
+][!is.na(iso3), .(iso3, age, mx = mxB)][
+  ref_demo_dt, on = .(iso3, age = from)
+]
+
+scaling <- ref_mx_dt[, sum(ifr_opts$SC2(age)*weight)/sum(mx*weight),]
+all_cause_mort <- ref_mx_dt[, approxfun(age, scaling*mx, rule = c(1:2))]
+
+ifr_opts$FLU <- all_cause_mort
+
+disease_pars <- list(
+  FLU = list(
+    infection_delays = c(0.8, 1.8), # incubation, typical duration infectious [days]
+    susc_par = 0.55,
+    transmissibility = 0.1
+  ),
+  SC2 = list(
+    infection_delays = c(3, 5),
+    susc_par = 0.3,
+    transmissibility = 0.1
+  )
+)
+
+save(ifr_opts, disease_pars, file = tail(.args, 1))
--- inst/R/param.R
+++ inst/R/param.R
@@ -1,67 +1,33 @@
 
 require(data.table)
 require(paramix)
-require(wpp2022)
-require(countrycode)
 require(socialmixr)
 
 .args <- if (interactive()) c(
   file.path("input", "population.rds"),
-  "GBR", "SC2",
+  file.path("input", "disease_pars.rda"),
+  "GBR",
+  "SC2",
   file.path("input", "param_GBR_FLU.rda")
 ) else commandArgs(trailingOnly = TRUE)
 
-ifr_opts <- list(
-  SC2 = \(age_in_years) {
-    scaled <- exp(-7.56 + 0.121 * age_in_years)
-    scaled / (100 + scaled)
-  }
-)
-
 pop_dt <- readRDS(.args[1])
-
-# TODO a bit slow + repeated - move into own script?
-# for FLU, going to assume looks like all-cause mortality in an HIC setting
-ref_demo_dt <- pop_dt[iso3 == "GBR"]
-data("mx1dt")
-ref_mx_dt <- mx1dt[
-  year == 2021
-][,
-  iso3 := countrycode(country_code, "iso3n", "iso3c")
-][!is.na(iso3), .(iso3, age, mx = mxB)][
-  ref_demo_dt, on = .(iso3, age = from)
-]
-
-scaling <- ref_mx_dt[, sum(ifr_opts$SC2(age)*weight)/sum(mx*weight),]
-all_cause_mort <- ref_mx_dt[, approxfun(age, scaling*mx, rule = c(1:2))]
-
-ifr_opts$FLU <- all_cause_mort
-
-disease_pars <- list(
-  FLU = list(
-    infection_delays = c(0.8, 1.8), # incubation, typical duration infectious [days]
-    susc_par = 0.55,
-    transmissibility = 0.1
-  ),
-  SC2 = list(
-    infection_delays = c(3, 5),
-    susc_par = 0.3,
-    transmissibility = 0.1
-  )
-)
+load(.args[2]) # ifr_opts
 
 # now downselect to focal parameters
 pop_dt <- pop_dt[
-  iso3 == pop_dt[, match.arg(.args[2], unique(iso3))]
+  iso3 == pop_dt[, match.arg(.args[3], unique(iso3))]
 ]
-f_ifr <- ifr_opts[[match.arg(.args[3], names(ifr_opts))]]
-sim_pars <- disease_pars[[match.arg(.args[3], names(ifr_opts))]]
+f_ifr <- ifr_opts[[match.arg(.args[4], names(ifr_opts))]]
+sim_pars <- disease_pars[[match.arg(.args[4], names(ifr_opts))]]
 
 model_agelimits <- c(0, 5, 20, 65, 101)
 
+# assume all age-contact patterns based off POLYMOD results
 cmij <- contact_matrix(
   polymod, survey.pop = pop_dt[, .(lower.age.limit = from, population = weight*1e3) ],
-  age.limits = model_agelimits
+  age.limits = model_agelimits, missing.participant.age = "remove",
+  missing.contact.age = "remove"
 )$matrix
 
 mapping_dt <- alembic(
@@ -70,7 +36,14 @@ mapping_dt <- alembic(
   new_partition = pop_dt[, seq(min(from), max(from) + 1L)]
 )
 
-ifr_params <- blend(mapping_dt)
+# using `parameter_summary` instead of `blend`, because we want to compare to
+# the naive alternatives
+ifr_params <- parameter_summary(
+  f_param = f_ifr, densities = pop_dt,
+  model_partition = model_agelimits
+)
+
+ifr_params[, model_from := model_agelimits[model_category]]
 
 save(
   model_agelimits, ifr_params, mapping_dt, cmij, sim_pars,
