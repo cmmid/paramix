4c114383ad8f76575b9b930d68841251969f8217
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2024-08-02 14:18:58 -0400

    update paramix parameter summary convenience calc - additional method, complete documentation

--- R/plotting.R
+++ R/plotting.R
@@ -1,16 +1,72 @@
 
-#' @title Convenient Parameter Comparison Summary
+#' @title Parameter Calculation Comparison Summary
+#'
+#' @description
+#' Implements several approaches to computing partition-aggregated parameters,
+#' then tables them up for convenient plotting.
+#'
 #'
 #' @inheritParams blend
 #'
 #' @param resolution the number of points to calculate for the underlying
-#' `f_param` line
+#' `f_param` function.
 #'
+#' @return a `data.table`, columns:
+#'  - `model_category`, a integer corresponding to which of the intervals of
+#'  `model_partition` the `x` value is in
+#'  - `x`, a numeric series from the first to last elements of `model_partition`
+#'  with length `resolution`
+#'  - `method`, a factor with levels:
+#'    * `f_val`: `f_param(x)`
+#'    * `f_mid`: `f_param(x_mid)`, where `x_mid` is the midpoint x of the
+#'    `model_category`
+#'    * `f_mean`: `f_param(weighted.mean(x, w))`, where `w` defined by
+#'    `densities` and `model_category`
+#'    * `mean_f`: `weighted.mean(f_param(x), w)`, same as previous
+#'    * `wm_f`: the result as if having used `paramix::blend()`; this should be
+#'    very similar to `mean_f`, though will be slightly different since `blend`
+#'    uses `integrate()`
+#'
+#' @examples
+#' # from Levin et al 2020 https://doi.org/10.1007/s10654-020-00698-1
+#' f_param <- function(age_in_years) {
+#'   scaled <- exp(-7.56 + 0.121 * age_in_years)
+#'   scaled / (100 + scaled)
+#' }
+#'
+#' densities <- data.frame(
+#'   from = 0:100,
+#'   weight = c(rep(1, 66), exp(-0.075 * 1:35))
+#' )
+#'
+#' model_partition <- c(0, 5, 20, 65, 101)
+#'
+#' ps_dt <- parameter_summary(f_param, densities, model_partition)
+#' ps_dt
+#'
+#' @examplesIf require(ggplot2)
+#' ggplot(ps_dt) + aes(x, y = value, color = method) +
+#'   geom_line(data = \(dt) subset(dt, method == "f_val")) +
+#'   geom_step(data = \(dt) subset(dt, method != "f_val")) +
+#'   theme_bw() + theme(
+#'     legend.position = "inside", legend.position.inside = c(0.05, 0.95),
+#'     legend.justification = c(0, 1)
+#'   ) + scale_color_discrete(
+#'     "Method", labels = c(
+#'       f_val = "f(x)", f_mid = "f(mid(x))", f_mean = "f(E[x])",
+#'       mean_f = "discrete E[f(x)]", wm_f = "integrated E[f(x)]"
+#'     )
+#'   ) +
+#'   scale_x_continuous("Age", breaks = seq(0, 100, by = 10)) +
+#'   scale_y_log10("IFR", breaks = 10^c(-6, -4, -2, 0), limits = 10^c(-6, 0))
+#'
+#' @importFrom data.table melt.data.table
 #' @export
 parameter_summary <- function(
   f_param, densities, model_partition, ...,
   resolution = diff(range(model_partition)) + 1L
 ) {
+
   partition <- make_partition(model_partition, open_partition = c(FALSE, FALSE))
 
   alembic_dt <- alembic(
@@ -19,23 +75,30 @@ parameter_summary <- function(
   )
 
   plot_dt <- data.table::data.table(
-    x = seq(partition[1], tail(partition, 1), length.out = resolution)
+    x = head(
+      seq(partition[1], tail(partition, 1), length.out = resolution), -1
+    )
   )[, f_val := f_param(x)]
 
   plot_dt[,
     model_category := findInterval(x, model_partition, all.inside = TRUE)
-  ]
+  ][alembic_dt, on = .(x = new_from), density := density]
 
   blended <- blend(alembic_dt)
 
-  plot_dt[, c("f_mean", "mean_f", "wm_f") := .(
-    f_param(mean(x)), mean(f_param(x)), blended[.GRP, value]
+  plot_dt[, c("f_mid", "f_mean", "mean_f", "wm_f") := .(
+    f_param(mean(x)),
+    f_param(weighted.mean(x, density)),
+    weighted.mean(f_param(x), density),
+    blended[.GRP, value]
   ), by = model_category]
 
-  data.table::melt.data.table(
-    plot_dt, id.vars = c("model_category", "x"),
-    variable.factor = FALSE, variable.name = "method"
-  )
+  plot_dt$density <- NULL
+
+  return(melt.data.table(
+    plot_dt,
+    id.vars = c("model_category", "x"), variable.name = "method"
+  ))
 
 }
 
