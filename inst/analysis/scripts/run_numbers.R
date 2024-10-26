62fc42eb6c80ec4040c5cf7b289d842b56f99aa5
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2024-10-26 18:34:56 -0400

    update analysis

--- inst/analysis/scripts/run_numbers.R
+++ inst/analysis/scripts/run_numbers.R
@@ -3,16 +3,16 @@ require(data.table)
 require(paramix)
 
 .args <- if (interactive()) c(
+  file.path("figure", "fig_utilities.rda"),
   file.path("input", "population.rds"),
   file.path("input", "disease_pars.rda"),
   file.path("output", "incidence.rds"),
-  file.path("output", "consolidated.rds"),
-  file.path("figure", "fig_utilities.rda")
+  file.path("output", "consolidated.rds")
 ) else commandArgs(trailingOnly = TRUE)
 
-load(.args[5])
+load(.args[1])
 
-inc_dt <- readRDS(.args[3])
+inc_dt <- readRDS(.args[4])
 model_partition <- c(inc_dt[, unique(model_from)], 101)
 inc_dt <- inc_dt[method == "f_mean"][,
                                      .(value = sum(value), capita = sum(unique(capita))),
@@ -20,8 +20,8 @@ inc_dt <- inc_dt[method == "f_mean"][,
 ]
 isoset <- inc_dt[, unique(iso3)]
 
-load(.args[2])
-pop_dt <- readRDS(.args[1])[iso3 %in% isoset]
+load(.args[3])
+pop_dt <- readRDS(.args[2])[iso3 %in% isoset]
 
 ifr_dt <- pop_dt[,
                  ifr_opts |> lapply(\(fp) parameter_summary(fp, .SD, model_partition)) |>
@@ -29,14 +29,14 @@ ifr_dt <- pop_dt[,
                  by = iso3
 ]
 
-print('SC2 65+ IFR')
-print('LMIC: [1] MEAN METHOD [2] PARAMIX')
-print(paste0(round(100*ifr_dt[x==100 & method %in% c('f_mean','wm_f') & pathogen=='SC2' & iso3=='AFG']$value, 1), '%'))
-print('HIC: [3] MEAN METHOD [4] PARAMIX')
-print(paste0(round(100*ifr_dt[x==100 & method %in% c('f_mean','wm_f') & pathogen=='SC2' & iso3=='GBR']$value, 1), '%'))
-print('')
+cat('SC2 65+ IFR', "\n")
+cat('LMIC: [1] MEAN METHOD [2] PARAMIX', "\n")
+cat(paste0(round(100*ifr_dt[x==100 & method %in% c('f_mean','wm_f') & pathogen=='SC2' & iso3=='AFG']$value, 1), '%'), "\n")
+cat('HIC: [3] MEAN METHOD [4] PARAMIX', "\n")
+cat(paste0(round(100*ifr_dt[x==100 & method %in% c('f_mean','wm_f') & pathogen=='SC2' & iso3=='GBR']$value, 1), '%'), "\n")
+cat('\n')
 
-ts_dt <- readRDS(.args[3])
+ts_dt <- readRDS(.args[4])
 
 joinvars <- c("method", "place", "pathogen")
 base_dt <- ts_dt[
@@ -50,22 +50,22 @@ int_dt <- ts_dt[intervention != "none", .(
 int_dt[, averted_death := i.deaths - deaths]
 int_dt$method <- factor(int_dt$method, levels=unique(int_dt$method))
 
-print('% OVERESTIMATE DEATHS COMPARED TO PARAMIX')
-print('SC2 LMIC: [1] VAX YOUNG [2] VAX WORKING [3] VAX OLDER')
-print(paste0(round(100*(int_dt[method=='f_mid'&pathogen=='SC2'&place=='AFG']$averted_death-int_dt[method=='wm_f'&pathogen=='SC2'&place=='AFG']$averted_death)/
-                     int_dt[method=='wm_f'&pathogen=='SC2'&place=='AFG']$averted_death, 2), '%'))
-print('FLU LMIC: [1] VAX YOUNG [2] VAX WORKING [3] VAX OLDER')
-print(paste0(round(100*(int_dt[method=='f_mid'&pathogen=='FLU'&place=='AFG']$averted_death-int_dt[method=='wm_f'&pathogen=='FLU'&place=='AFG']$averted_death)/
-                     int_dt[method=='wm_f'&pathogen=='FLU'&place=='AFG']$averted_death, 2), '%'))
-print('SC2 HIC: [1] VAX YOUNG [2] VAX WORKING [3] VAX OLDER')
-print(paste0(round(100*(int_dt[method=='f_mid'&pathogen=='SC2'&place=='GBR']$averted_death-int_dt[method=='wm_f'&pathogen=='SC2'&place=='GBR']$averted_death)/
-                     int_dt[method=='wm_f'&pathogen=='SC2'&place=='GBR']$averted_death, 2), '%'))
-print('FLU HIC: [1] VAX YOUNG [2] VAX WORKING [3] VAX OLDER')
-print(paste0(round(100*(int_dt[method=='f_mid'&pathogen=='FLU'&place=='GBR']$averted_death-int_dt[method=='wm_f'&pathogen=='FLU'&place=='GBR']$averted_death)/
-                     int_dt[method=='wm_f'&pathogen=='FLU'&place=='GBR']$averted_death, 2), '%'))
-print('')
+cat('% OVERESTIMATE DEATHS COMPARED TO PARAMIX', "\n")
+cat('SC2 LMIC: [1] VAX YOUNG [2] VAX WORKING [3] VAX OLDER', "\n")
+cat(paste0(round(100*(int_dt[method=='f_mid'&pathogen=='SC2'&place=='AFG']$averted_death-int_dt[method=='wm_f'&pathogen=='SC2'&place=='AFG']$averted_death)/
+                     int_dt[method=='wm_f'&pathogen=='SC2'&place=='AFG']$averted_death, 2), '%'), "\n")
+cat('FLU LMIC: [1] VAX YOUNG [2] VAX WORKING [3] VAX OLDER', "\n")
+cat(paste0(round(100*(int_dt[method=='f_mid'&pathogen=='FLU'&place=='AFG']$averted_death-int_dt[method=='wm_f'&pathogen=='FLU'&place=='AFG']$averted_death)/
+                     int_dt[method=='wm_f'&pathogen=='FLU'&place=='AFG']$averted_death, 2), '%'), "\n")
+cat('SC2 HIC: [1] VAX YOUNG [2] VAX WORKING [3] VAX OLDER', "\n")
+cat(paste0(round(100*(int_dt[method=='f_mid'&pathogen=='SC2'&place=='GBR']$averted_death-int_dt[method=='wm_f'&pathogen=='SC2'&place=='GBR']$averted_death)/
+                     int_dt[method=='wm_f'&pathogen=='SC2'&place=='GBR']$averted_death, 2), '%'), "\n")
+cat('FLU HIC: [1] VAX YOUNG [2] VAX WORKING [3] VAX OLDER', "\n")
+cat(paste0(round(100*(int_dt[method=='f_mid'&pathogen=='FLU'&place=='GBR']$averted_death-int_dt[method=='wm_f'&pathogen=='FLU'&place=='GBR']$averted_death)/
+                     int_dt[method=='wm_f'&pathogen=='FLU'&place=='GBR']$averted_death, 2), '%'), "\n")
+cat('\n')
 
-ylls_dt <- readRDS(.args[4])
+ylls_dt <- readRDS(.args[5])
 base_dt <- ylls_dt[
   intervention == "none", .SD, .SDcols = -c("intervention")
 ]
@@ -77,12 +77,12 @@ int_dt[, sim_method := factor(sim_method, levels = names(model_assumption_labels
 int_dt[, method := factor(method, levels = names(model_assumption_labels), ordered = TRUE)]
 int_dt <- int_dt[sim_method=='wm_f'][, sim_method:=NULL]
 
-print('% OVERESTIMATE YLLS COMPARED TO PARAMIX')
-print('LMIC: [1] SC2 [2] FLU')
-print(paste0(100*round((int_dt[method=='mean_f' & intervention=='vax_working' & place=='AFG']$averted_yll -
+cat('% OVERESTIMATE YLLS COMPARED TO PARAMIX', "\n")
+cat('LMIC: [1] SC2 [2] FLU', "\n")
+cat(paste0(100*round((int_dt[method=='mean_f' & intervention=='vax_working' & place=='AFG']$averted_yll -
                       int_dt[method=='wm_f' & intervention=='vax_working' & place=='AFG']$averted_yll)/
-                     int_dt[method=='wm_f' & intervention=='vax_working' & place=='AFG']$averted_yll,2), '%'))
-print('HIC: [1] SC2 [2] FLU')
-print(paste0(100*round((int_dt[method=='mean_f' & intervention=='vax_working' & place=='GBR']$averted_yll -
+                     int_dt[method=='wm_f' & intervention=='vax_working' & place=='AFG']$averted_yll,2), '%'), "\n")
+cat('HIC: [1] SC2 [2] FLU', "\n")
+cat(paste0(100*round((int_dt[method=='mean_f' & intervention=='vax_working' & place=='GBR']$averted_yll -
                           int_dt[method=='wm_f' & intervention=='vax_working' & place=='GBR']$averted_yll)/
-                         int_dt[method=='wm_f' & intervention=='vax_working' & place=='GBR']$averted_yll,2), '%'))
+                         int_dt[method=='wm_f' & intervention=='vax_working' & place=='GBR']$averted_yll,2), '%'), "\n")
