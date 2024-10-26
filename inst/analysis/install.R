7d5e46f5afe942a94bd8486c5f1dc48756163261
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2024-10-26 17:42:31 -0400

    renv troubleshooting

--- inst/analysis/install.R
+++ inst/analysis/install.R
@@ -2,6 +2,12 @@
 stopifnot("Must have `renv` installed." = require(renv))
 
 renv::activate()
-res <- renv::restore(prompt = FALSE)
+tryCatch(
+  res <- renv::restore(prompt = FALSE),
+  error = function(e) {
+    warning(e)
+    renv::deactivate()
+  }
+)
 
 print(res)
